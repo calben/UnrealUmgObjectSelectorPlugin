15763b4fc2611e7e26bbd66bacd4c6d0f0daaf81
Calem Bendell <calem.j.bendell@gmail.com>
2020-12-19 18:21:30 -0500

    module rules fix for ue426

--- Source/UmgObjectSelector/Public/UmgObjectSelector.h
+++ Source/UmgObjectSelector/Public/UmgObjectSelector.h
@@ -1,15 +1,18 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "ModuleManager.h"
+#include "Modules/ModuleManager.h"
 
 class FUmgObjectSelectorModule : public IModuleInterface
 {
 public:
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+    /** IModuleInterface implementation */
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+private:
+    /** Handle to the test dll we will load */
+    void* ExampleLibraryHandle;
 };
